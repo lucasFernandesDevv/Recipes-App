036571cc4078451f959ae6dbf96e3195e472874a
Douglas Araújo <devdoug45@gmail.com>
2023-05-05 17:03:43 -0300

    style: cria o estilo dos inputs da tela de login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/">
-        <h1>Home</h1>
+        <Login />
       </Route>
     </Switch>
   );
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,18 +2,51 @@ import React from 'react';
 
 export default function Login() {
   return (
-    <main>
-      <form>
+    <main className="flex justify-center items-center h-screen">
+      <form className="flex flex-col justify-center gap-4 items-center">
+        <h1 className="text-xl text-violet-700 italic">LOGIN</h1>
         <label htmlFor="email">
-          Email
-          <input type="email" name="email" id="email" />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            data-testid="email-input"
+            className="
+              border-2
+              border-violet-300
+              outline-none
+              placeholder-violet-300
+              ml-2
+              w-64
+              p-2
+              "
+          />
         </label>
         <label htmlFor="password">
-          Senha
-          <input type="password" name="password" id="password" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            data-testid="password-input"
+            className="
+            border-2
+            border-violet-300
+            outline-none
+            placeholder-violet-300
+            ml-2
+            w-64
+            p-2
+            "
+          />
         </label>
-        <button type="submit">
-          Entrar
+        <button
+          type="submit"
+          data-testid="login-submit-btn"
+          className="w-64 ml-2 bg-yellow-400 rounded-md text-white font-extrabold p-2"
+        >
+          ENTER
         </button>
       </form>
     </main>
