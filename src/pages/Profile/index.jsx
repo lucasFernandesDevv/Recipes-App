52533cb722ebec07b2dff83ced1e36b334c86b18
gabri10on <gabriel.r_00@hotmail.com>
2023-05-09 19:17:54 -0300

    feat: requirements 60, 61, 62 -> finalized profile page [missing tests]

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
 function Profile() {
   const user = JSON.parse(localStorage.getItem('user'));
-  // const doneRecipes = [{
-  //   id: 'id - da - receita',
-  //   type: 'meal - ou - drink',
-  //   nationality: 'nacionalidade - da - receita - ou - texto - vazio',
-  //   category: 'categoria - da - receita - ou - texto - vazio',
-  //   alcoholicOrNot: 'alcoholic - ou - non - alcoholic - ou - texto - vazio',
-  //   name: 'nome - da - receita',
-  //   image: 'imagem - da - receita',
-  //   doneDate: 'quando - a - receita - foi - concluida',
-  //   tags: 'array - de - tags - da - receita - ou - array - vazio',
-  // }];
 
-  localStorage.setItem('doneRecipes', JSON.stringify(doneRecipes));
+  const handleLogout = () => {
+    localStorage.clear();
+  };
 
   return (
     <>
@@ -26,17 +18,23 @@ function Profile() {
           {user.email}
         </span>
         <section>
-          <button data-testid="profile-done-btn">
-            <span>Done Recipes</span>
-          </button>
+          <Link to="/done-recipes">
+            <button data-testid="profile-done-btn">
+              Done Recipes
+            </button>
+          </Link>
           <hr />
-          <button data-testid="profile-favorite-btn">
-            <span>Favorite Recipes</span>
-          </button>
+          <Link to="/favorite-recipes">
+            <button data-testid="profile-favorite-btn">
+              Favorite Recipes
+            </button>
+          </Link>
           <hr />
-          <button data-testid="profile-logout-btn">
-            <span>Logout</span>
-          </button>
+          <Link to="/">
+            <button data-testid="profile-logout-btn" onClick={ () => handleLogout() }>
+              Logout
+            </button>
+          </Link>
         </section>
       </div>
       <Footer />
