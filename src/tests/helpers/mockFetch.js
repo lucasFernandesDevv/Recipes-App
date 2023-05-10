187cf726078122ec46ec56425b3aff5f91914379
butzlaff <emiliobutz@gmail.com>
2023-05-10 20:11:59 -0300

    atualizando tests

--- src/tests/helpers/mockFetch.js
+++ src/tests/helpers/mockFetch.js
@@ -1,4 +1,10 @@
-export const mockFetch = (data) => jest.fn(() => Promise.resolve({
-  ok: true,
-  json: () => Promise.resolve(data),
-}));
+// export const mockFetch = (data) => jest.fn(() => Promise.resolve({
+//   ok: true,
+//   json: () => Promise.resolve(data),
+// }));
+
+import fetch from '../../../cypress/mocks/fetch';
+
+export const mockFetch = () => {
+  jest.spyOn(global, 'fetch').mockImplementation(fetch);
+};
