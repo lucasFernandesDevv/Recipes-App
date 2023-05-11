9e19226358cdb2eed3bf5f3f69d94a9cf029fe51
Douglas Araújo <devdoug45@gmail.com>
2023-05-11 16:43:29 -0300

    feat: filtra as receitas feitas por tipo de receita

--- src/components/FilterButtons/index.jsx
+++ src/components/FilterButtons/index.jsx
@@ -1,11 +1,31 @@
+import PropTypes from "prop-types"
 import React from 'react';
 
-export function FilterButtons() {
+export function FilterButtons({ setFilteredDoneRecipes }) {
+  const buttonName = ['Meals', 'Drinks', 'All'];
+
+  function handleFilter(filter) {
+    const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+    const filteredRecipes = doneRecipes.filter(({ type }) => type === filter);
+    setFilteredDoneRecipes(filteredRecipes);
+  }
+
   return (
     <div>
-      <button>Meals</button>
-      <button>Drinks</button>
-      <button>All</button>
+      {buttonName.map((name, index) => (
+        <button
+          key={ `${index}-${name}` }
+          data-testid={ `${name}-filter` }
+          id={ name }
+          onClick={ ({ target }) => handleFilter(target.id) }
+        >
+          {name}
+        </button>
+      ))}
     </div>
-  )
+  );
 }
+
+FilterButtons.propTypes = {
+  setFilteredDoneRecipes: PropTypes.func,
+};
--- src/pages/DoneRecipes/index.jsx
+++ src/pages/DoneRecipes/index.jsx
@@ -1,40 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import { DoneRecipeCard } from '../../components/DoneRecipeCard';
+import { FilterButtons } from '../../components/FilterButtons';
 
 function DoneRecipes() {
   const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+  const [filteredDoneRecipes, setFilteredDoneRecipes] = useState([]);
+
+  const hasAnyFilter = filteredDoneRecipes.length > 0;
+
   return (
     <>
       <Header title="Done Recipes" />
-      {doneRecipes.map(
-        (
-          {
-            name,
-            doneDate,
-            tags,
-            nationality,
-            category,
-            isAlcoholic,
-            recipeType,
-            id,
-          },
-          i,
-        ) => (
-          <DoneRecipeCard
-            key={ name }
-            doneDate={ doneDate }
-            tags={ tags }
-            nationality={ nationality }
-            category={ category }
-            isAlcoholic={ isAlcoholic }
-            recipeType={ recipeType }
-            index={ i }
-            id={ id }
-          />
-        ),
-      )}
+      <FilterButtons setFilteredDoneRecipes={ setFilteredDoneRecipes } />
+      <div>
+        {(hasAnyFilter ? filteredDoneRecipes : doneRecipes).map(
+          (
+            {
+              name,
+              doneDate,
+              tags,
+              nationality,
+              category,
+              isAlcoholic,
+              recipeType,
+              id,
+            },
+            i,
+          ) => (
+            <DoneRecipeCard
+              key={ name }
+              doneDate={ doneDate }
+              tags={ tags }
+              nationality={ nationality }
+              category={ category }
+              isAlcoholic={ isAlcoholic }
+              recipeType={ recipeType }
+              index={ i }
+              id={ id }
+            />
+          ),
+        )}
+      </div>
       <Footer />
     </>
   );
